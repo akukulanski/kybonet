b058bbbaddeca51b5baf41c66b9216fb92fb08d1
akukulanski <akukulanski@gmail.com>
2021-03-26 19:50:37 -0300

    kybonet/mouse_dev.py: cleanup

--- kybonet/mouse_dev.py
+++ kybonet/mouse_dev.py
@@ -1,61 +1,19 @@
 import evdev
 import time
-from collections import namedtuple
-from evdev import UInput, AbsInfo, ecodes
-
-
-class MouseButtonEvent:
-    def __init__(self, event_type, button):
-        self.event_type = event_type
-        self.button = button
-        self.time = time.time()
-
-    def __str__(self):
-        fmt = 'MouseButtonEvent(button={}, event_type={}, time={})'
-        return fmt.format(self.button, self.event_type, self.time)
-
-
-class MouseWheelEvent:
-    def __init__(self, delta):
-        self.delta = delta
-        self.time = time.time()
-
-    def __str__(self):
-        fmt = 'MouseWheelEvent(delta={}, time={})'
-        return fmt.format(self.delta, self.time)
-
-
-class MouseMoveEvent:
-    def __init__(self, x_rel, y_rel):
-        self.x_rel = x_rel
-        self.y_rel = y_rel
-        self.time = time.time()
-
-    def __str__(self):
-        fmt = 'MouseMoveEvent(x_rel={}, y_rel={}, time={})'
-        return fmt.format(self.x_rel, self.y_rel, self.time)
-
-
-class MouseMoveEventX(MouseMoveEvent):
-    def __init__(self, x_rel):
-        MouseMoveEvent.__init__(self, x_rel, 0)
-
-
-class MouseMoveEventY(MouseMoveEvent):
-    def __init__(self, y_rel):
-        MouseMoveEvent.__init__(self, 0, y_rel)
-
+from evdev import UInput, ecodes
 
 _ev_key_codes = {ecodes.BTN_MOUSE: 'left',
                  ecodes.BTN_RIGHT: 'right',
                  ecodes.BTN_MIDDLE: 'middle',
                  ecodes.BTN_SIDE: 'side',
                  ecodes.BTN_EXTRA: 'extra'}
-_ev_key_types = {0: 'up', 1: 'down'}
 
-_ev_rel_codes = {ecodes.REL_X: MouseMoveEventX,
-                 ecodes.REL_Y: MouseMoveEventY,
-                 ecodes.REL_WHEEL: MouseWheelEvent}
+_ev_key_types = {0: 'up',
+                 1: 'down'}
+
+_ev_rel_codes = {ecodes.REL_X: 'deltaX',
+                 ecodes.REL_Y: 'deltaY',
+                 ecodes.REL_WHEEL: 'deltaWheel'}
 
 
 class MouseEvent:
@@ -64,7 +22,6 @@ class MouseEvent:
         self.code = code
         self.value = value
         self.time = time
-        self.parsed = self.parse()
 
     @classmethod
     def from_event(cls, event):
@@ -75,21 +32,21 @@ class MouseEvent:
             if self.code in _ev_rel_codes:
                 return True
         if self.etype == ecodes.EV_KEY:
-            if (    self.code in _ev_key_codes and
-                    self.value in _ev_key_types):
+            if (self.code in _ev_key_codes and self.value in _ev_key_types):
                 return True
         return False
 
-    def parse(self):
-        if self.is_valid():
-            if self.etype == ecodes.EV_REL:
-                _mouse_event_rel = _ev_rel_codes[self.code]
-                return _mouse_event_rel(self.value)
-            if self.etype == ecodes.EV_KEY:
-                button = _ev_key_codes[self.code]
-                event_type = _ev_key_types[self.value]
-                return MouseButtonEvent(event_type=event_type, button=button)
-        return None
+    def __str__(self):
+        if self.etype == ecodes.EV_REL:
+            fmt = 'MouseRelative(x={}, y={}, wheel={}, time={})'
+            x, y, wheel = self.get_rel_movement()
+            return fmt.format(x, y, wheel, self.time)
+        elif self.etype == ecodes.EV_KEY:
+            fmt = 'MouseKey(button="{}", action="{}", time={})'
+            button = _ev_key_codes[self.code]
+            action = _ev_key_types[self.value]
+            return fmt.format(button, action, self.time)
+        return 'MouseUnknown()'
 
     def is_rel_movement(self):
         return self.etype == ecodes.EV_REL
@@ -143,7 +100,6 @@ class RelativeMovement:
             events.append(MouseEvent(etype=ecodes.EV_REL, code=ecodes.REL_WHEEL,
                                      value=self.wheel, time=self.time))
         return events
-
 
 
 class FakeMouse:
