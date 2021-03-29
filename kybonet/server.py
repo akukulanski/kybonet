0e5ff458ab102f62e8b4b8bad70a7dcb9c088244
akukulanski <akukulanski@gmail.com>
2021-03-28 23:54:42 -0300

    kybonet/server.py: refactor

--- kybonet/server.py
+++ kybonet/server.py
@@ -3,6 +3,7 @@ import json
 import yaml
 import zmq
 import logging
+import sys
 import os
 from collections import defaultdict
 from selectors import DefaultSelector, EVENT_READ
@@ -22,173 +23,149 @@ def parse_args(args=None):
     parser.add_argument('-c', '--config', type=str,
                         default=default_config_file,
                         help='YML configuration file')
+    verbosity = parser.add_mutually_exclusive_group(required=False)
+    verbosity.add_argument('-q', '--quiet', action='store_true',
+                           help='Reduce output messages.')
+    verbosity.add_argument('-v', '--verbose', action='store_true',
+                           help='Increment output messages.')
     return parser.parse_args(args)
 
 
-def load_subscribers(subs):
-    """
-    Reads the public key of every subscriber and stores it in the field
-    'public_key'.
-    """
-    loaded_subs = []
-    for s in subs:
-        if 'key' not in s or not s['key']:
-            s['public_key'] = None
-            s['is_local'] = True
-            loaded_subs.append(s)
-            logger.info('Client with no key: local usage of the devices')
-        else:
-            try:
-                with open(s['key'], 'rb') as f:
-                    key = import_public_key(f.read())
-                s['public_key'] = key
-                s['is_local'] = False
-                loaded_subs.append(s)
-                logger.info('Successfully imported key for "{}"'.format(s['name']))
-            except Exception as e:
-                logger.error(e)
-                logger.warning('Error importing key for "{}"'.format(s['name']))
-    return loaded_subs
+class KybonetServer:
 
+    def __init__(self):
+        # zmq
+        self._context = None
+        self._socket = None
+        # subs
+        self._subs = []
+        # devices
+        self._devices_connected = []
+        self._devices = []
+        # hotkeys
+        self._hotkeys = self._empty_hotkeys()
+        # run
+        self._selector = DefaultSelector()
+        # state
+        self._current_idx = 0
+        self._pressed_keys = defaultdict(lambda: False)
+        self._grabbed = False
 
-class State:
-    """
-    Class to track the current state.
-    """
-    def __init__(self, subscribers, devices):
-        self._subscribers = subscribers
-        self.current_idx = 0
-        self.last_event = None
-        self.pressed_keys = defaultdict(lambda: False)
-        self.devices = devices
-        self.grabbed = False
-        self.switch(idx=0)
-
-    def next(self):
-        next_idx = (self.current_idx + 1) % len(self._subscribers)
-        self.switch(next_idx)
-
-    def switch(self, idx):
-        if idx < len(self._subscribers):
-            self.current_idx = idx
-            if self.current_sub['is_local']:
-                self.ungrab_all()
-            else:
-                self.grab_all()
-            logger.info('Jumped to sub: {} ({})'.format(
-                self.current_idx,
-                self.current_sub['name']))
-        else:
-            logger.warning('Ignoring invalid sub: {}'.format(idx))
+    # def __del__(self):
+    #     self.ungrab_all()
 
     @property
-    def current_sub(self):
-        return self._subscribers[self.current_idx]
+    def subs(self):
+        return self._subs
 
-    def grab_all(self):
-        if self.grabbed:
-            return
-        logger.debug('grab()')
-        self.grabbed = True
-        for d in self.devices:
-            d.grab()
+    @property
+    def devices(self):
+        return self._devices
 
-    def ungrab_all(self):
-        if not self.grabbed:
-            return
-        logger.debug('ungrab()')
-        self.grabbed = False
-        for d in self.devices:
-            d.ungrab()
+    def connect(self, port):
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.PUB)
+        self._socket.bind("tcp://*:{}".format(port))
+        logger.info('Running zmq publisher on port {}'.format(port))
 
-    def __del__(self):
-        self.ungrab_all()
+    def add_subscriber(self, name, id_file=None, hotkey=None):
+        new_sub = {'name': name, 'public_key': None, 'is_local': False,
+                   'hotkey': None}
+        if id_file is not None:
+            with open(id_file, 'rb') as f:
+                key = import_public_key(f.read())
+            new_sub['public_key'] = key
+            new_sub['is_local'] = False
+        else:
+            new_sub['is_local'] = True
+        if hotkey:
+            new_sub['hotkey'] = hotkey
+            k = 'switch_' + name
+            self._hotkeys[k] = {'key': keycode_from_str(hotkey),
+                                'callback': self.switch,
+                                'args': (len(self._subs),)}
+        self._subs.append(new_sub)
+        logger.debug('Client added: {}'.format(name))
 
+    def scan_devices(self):
+        self._devices_connected = find_devices()
 
-def main(args=None):
-    debug = os.getenv('DEBUG', None)
-    level = logging.DEBUG if debug else logging.INFO
-    logging.basicConfig(level=level)
-    logger = logging.getLogger()
+    def add_device(self, name):
+        for d in self._devices_connected:
+            if name == d.name:
+                self._devices.append(d)
+                logger.debug('Found device "{}"'.format(name))
+                return
+        raise RuntimeError('Device "{}" not present'.format(name))
 
-    args = parse_args(args=args)
-    port = args.port
-    context = zmq.Context()
-    socket = context.socket(zmq.PUB)
-    socket.bind("tcp://*:{}".format(port))
-    logger.info('running zmq publisher on port {}'.format(port))
+    def _empty_hotkeys(self):
+        return {'switch': {'key': None,
+                           'callback': self.next,
+                           'args': ()},
+                'exit': {'key': None,
+                         'callback': self.exit_program,
+                         'args': ('Exit key pressed',)},}
 
-    with open(args.config, 'r') as f:
-        config = yaml.load(f.read(), Loader=yaml.FullLoader)
-
-    subs = config['subscribers']
-    subs = load_subscribers(subs)
-    assert len(subs), 'No subscribers available'
-
-    devices = []
-    for d in find_devices():
-        if d.name in config['devices']:
-            logger.info('Found device: {}'.format(d.name))
-            devices.append(d)
-            d.read()
-    assert len(devices), 'No devices found'
-    selector = DefaultSelector()
-    for d in devices:
-        selector.register(d, EVENT_READ)
-
-    state = State(subs, devices=devices)
-    hotkeys = []
-
-    def add_hotkey(key, callback, args):
+    def assign_hotkey(self, name, key):
+        assert name in self._hotkeys, 'Unknown hotkey: "{}"'.format(name)
         if isinstance(key, int):
             keycode = key
         elif isinstance(key, str):
             keycode = keycode_from_str(key)
         assert isinstance(keycode, int), 'Invalid key: "{}"'.format(key)
-        new_hotkey = (keycode, callback, args)
-        hotkeys.append(new_hotkey)
-        return new_hotkey
+        self._hotkeys[name]['key'] = keycode
 
-    def exit_program(reason='-'):
-        logger.info('Exit ({})'.format(reason))
-        for k, pressed in state.pressed_keys.items():
+    def teardown(self):
+        logger.debug('Teardown')
+        for k, pressed in self._pressed_keys.items():
             if pressed:
                 new_event = PseudoEvent.KeyRelease(k)
-                send_event(new_event)
-        state.ungrab_all()
-        for d in devices:
-            selector.unregister(d)
-        exit(0)
+                self.send_event(new_event)
+        self.ungrab_all()
+        for d in self._devices:
+            self._selector.unregister(d)
+        self._selector.close()
 
-    # assign hotkeys
-    _hotkeys_config = [
-        ('switch_hotkey', state.next, ()),
-        ('exit_hotkey', exit_program, ('Exit key pressed',)),
-    ]
+    def exit_program(self, reason='-'):
+        self.teardown()
+        logger.info('Exit.')
+        os._exit(0)
 
-    _hotkeys_to_assign = []
+    def next(self):
+        next_idx = (self._current_idx + 1) % len(self._subs)
+        self.switch(next_idx)
 
-    for name, callback, args in _hotkeys_config:
-        if name in config and config[name]:
-            _hotkeys_to_assign.append((config[name], callback, args))
+    def switch(self, idx):
+        if idx < len(self._subs):
+            self._current_idx = idx
+            if self.current_sub['is_local']:
+                self.ungrab_all()
+            else:
+                self.grab_all()
+            logger.info('Current sub: {} ({})'.format(
+                self.current_sub['name'], self._current_idx,))
         else:
-            logger.warning('No key assigned for "{}"'.format(name))
+            logger.warning('Ignoring invalid sub: {}'.format(idx))
 
-    for i in range(len(subs)):
-        s = subs[i]
-        if 'hotkey' in s and s['hotkey']:
-            _hotkeys_to_assign.append((s['hotkey'], state.switch, (i,)))
+    @property
+    def current_sub(self):
+        return self._subs[self._current_idx]
 
-    for key, callback, args in _hotkeys_to_assign:
-        try:
-            add_hotkey(key=key, callback=callback, args=args)
-        except AssertionError as e:
-            logger.error(e)
-            exit_program(reason=str(e))
-        fmt = 'Key assigned for "{}": "{}"'
-        logger.info(fmt.format(config[name], config[name]))
+    def grab_all(self):
+        if self._grabbed:
+            return
+        self._grabbed = True
+        for d in self._devices:
+            d.grab()
 
-    def merge_events(events):
+    def ungrab_all(self):
+        if not self._grabbed:
+            return
+        self._grabbed = False
+        for d in self._devices:
+            d.ungrab()
+
+    def merge_events(self, events):
         merged_events = []
         rel_movement = RelativeMovement()
         for e in events:
@@ -207,82 +184,119 @@ def main(args=None):
         rel_movement.set(0, 0, 0)
         return merged_events
 
-    def matches_hotkey(event):
-        for key, _, __ in hotkeys:
-            if key == event.code:
+    def event_is_hotkey(self, event):
+        for name, hotkey in self._hotkeys.items():
+            if hotkey['key'] == event.code:
                 return True
         return False
 
-    def is_hotkey_down(event):
-        return event.is_key_pressed() and matches_hotkey(event)
-
-    def is_hotkey_up(event):
-        return event.is_key_released() and matches_hotkey(event)
-
-    def remove_hostkey_press(events):
-        return [e for e in events if not(is_hotkey_down(e))]
-
-    def run_hotkey_callback(event):
-        for key, callback, args in hotkeys:
-            if key == event.code:
-                callback(*args)
+    def run_hotkey_callback(self, event):
+        for name, hotkey in self._hotkeys.items():
+            # key, callback, args
+            if hotkey['key'] == event.code:
+                hotkey['callback'](*hotkey['args'])
                 return
-        keys = [h[0] for h in hotkeys]
-        logger.warning('Key {} not found in hotkeys {}'.format(key, keys))
+        raise RuntimeError('Key code {} is not a hotkey.'.format(event.ecode))
 
-    def parse_events(device, events):
+    def parse_events(self, device, events):
         events = [PseudoEvent.from_event(e) for e in events]
         if is_mouse(device):
             events = [e for e in events if e.is_valid_mouse_event()]
-            events = merge_events(events)
+            events = self.merge_events(events)
         else:
             events = [e for e in events if e.is_valid_keyboard_event()]
-            events = remove_hostkey_press(events)
+            events = [e for e in events if (
+                        not(e.is_key_pressed() and self.event_is_hotkey(e)))]
         for event in events:
-            if is_hotkey_up(event):
+            if self.event_is_hotkey(event) and event.is_key_released():
                 logger.debug('Hotkey detected ({}).'.format(event.code))
-                for k, pressed in state.pressed_keys.items():
+                for k, pressed in self._pressed_keys.items():
                     if pressed:
                         new_event = PseudoEvent.KeyRelease(k)
-                        send_event(new_event)
-                run_hotkey_callback(event)
+                        self._send_event(new_event)
+                self.run_hotkey_callback(event)
             else:
-                send_event(event)
+                self._send_event(event)
         return
 
-    def send_event(event):
-        if state.current_sub['is_local']:
+    def _send_event(self, event):
+        if self.current_sub['is_local']:
             logger.debug('Local user, nothing done.')
             return
 
-        if event.is_key_pressed() and state.pressed_keys[event.code]:
+        if event.is_key_pressed() and self._pressed_keys[event.code]:
             return
 
         if event.is_key_pressed():
-            state.pressed_keys[event.code] = True
+            self._pressed_keys[event.code] = True
         elif event.is_key_released():
-            state.pressed_keys[event.code] = False
+            self._pressed_keys[event.code] = False
 
         fields = ['etype', 'code', 'value', 'time']
         event_dict = {k: getattr(event, k) for k in fields}
         to_send_str = json.dumps(event_dict)
         encrypted = encrypt(message=to_send_str.encode('utf-8'),
-                            public_key=state.current_sub['public_key'])
-        socket.send(encrypted)
+                            public_key=self.current_sub['public_key'])
+        self._socket.send(encrypted)
 
-    logger.info('Key event publisher is now active.')
-    logger.info('Current subscriber: {}'.format(state.current_sub['name']))
+    def run(self):
+        for d in self._devices:
+            self._selector.register(d, EVENT_READ)
 
-    try:
-        while True:
-            for key, mask in selector.select():
-                device = key.fileobj
-                events = list(device.read())
-                parse_events(device, events)
-    except KeyboardInterrupt:
-        pass
+        logger.info('Kybonet server is now active.')
+        logger.info('Current subscriber is: {}'.format(
+                self.current_sub['name']))
 
-    state.ungrab_all()
+        self.switch(idx=0)
+        try:
+            while True:
+                for key, mask in self._selector.select():
+                    device = key.fileobj
+                    events = list(device.read())
+                    self.parse_events(device, events)
+        except KeyboardInterrupt:
+            pass
+
+
+def main(args=None):
+    args = parse_args(args=args)
+
+    if args.quiet:
+        logging.basicConfig(level=logging.ERROR)
+    elif args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    with open(args.config, 'r') as f:
+        config = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+    server = KybonetServer()
+    server.connect(port=args.port)
+
+    for s in config['subscribers']:
+        server.add_subscriber(**s)
+
+    if len(server.subs) == 0:
+        logger.error('No subscribers available.')
+        sys.exit(1)
+
+    server.scan_devices()
+    for d in config['devices']:
+        try:
+            server.add_device(d)
+        except RuntimeError as e:
+            logger.error(e)
+
+    if len(server.devices) == 0:
+        logger.error('No devices available.')
+        sys.exit(1)
+
+    for name, key in config['hotkeys'].items():
+        if key:
+            server.assign_hotkey(name, key)
+
+    server.run()
 
 
 if __name__ == '__main__':
